e74ed57cd1171dbb85ca5a15cf659b8102d80c1f
sonatine726 <sonatine726@gmail.com>
2018-04-29 22:00:54 +0900

    Change host to localhost because of error in sending email for user set up.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "app_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
-    host: 'kommet.herokuapp.com'
+    host: 'localhost'
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
