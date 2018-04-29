7c11bf1392fb6b22cdbd4bd3432d4864a8c364fb
sonatine726 <sonatine726@gmail.com>
2018-04-29 16:29:27 +0900

    Fix a bug and refacto codes.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,12 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.read_encrypted_secrets = true
+
+  config.logger = Logger.new('log/development.log')
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -39,10 +43,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
+    domain: 'gmail.com',
     port: 587,
     authentication: :plain,
     user_name: Rails.application.secrets.SMTP_EMAIL,
-    password: Rails.application.secrets.SMTP_PASSWORD
+    password: Rails.application.secrets.SMTP_PASSWORD,
+    enable_starttls_auto: true
   }
 
 
