6bd4135a350adf88a5578a2ae49e2761728b39ed
sonatine726 <sonatine726@gmail.com>
2018-05-01 12:07:38 +0900

    Commit intermediate.(Add functions to model 'User'.)

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,9 +26,26 @@
 #
 
 class User < ApplicationRecord
+  attr_accessor: login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    conditions[:email].downcase! if conditions[:email]
+    login = conditions.delete(:login)
+
+    where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value, {value: login.downcase}"]).first
 end
