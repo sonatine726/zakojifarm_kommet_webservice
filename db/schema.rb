300012bea972becc0c1525e5f87616b38c7560e8
sonatine726 <sonatine726@gmail.com>
2018-05-07 00:00:22 +0900

    Add role attribute to model 'User'.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506023129) do
+ActiveRecord::Schema.define(version: 20180506145541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20180506023129) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer "posts_count", default: 0, null: false
+    t.integer "role", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
