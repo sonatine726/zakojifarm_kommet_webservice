12514b91069e00948c43a2b3901cdcbc97685bc2
sonatine726 <sonatine726@gmail.com>
2018-05-07 00:24:47 +0900

    Add function to authenticate role for view admin pages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from SecurityError do |exception|
+    redirect_to root_url, notice: 'Admin画面へのアクセス権限がありません'
+  end
+
   protected
 
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me,:avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def authenticate_admin_user!
+    raise SecurityError unless current_user.try(:admin?)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
   end
 
   def flash_message(message, klass)
-    content_tag(:div, class: 'alert alert-#{klass}') do
+    content_tag(:div, class: "alert alert-#{klass}") do
       concat content_tag(:button, 'x', class: 'close', data: {dismiss: 'alert'})
       concat raw(message)
     end
