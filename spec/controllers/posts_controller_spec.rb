725ad3b78e4a649c43f9da78df27df5d11c03c64
sonatine726 <sonatine726@gmail.com>
2018-05-08 12:22:06 +0900

    Commit intermediate.(Fixing tests.)

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
   describe "GET #create" do
-    it "returns http success" do
-      pending 'Not update test yet.'
-      get :create
-      expect(response).to have_http_status(:success)
+    context "valid post" do
+      let(:user){FactoryBot.create(:user)}
+      before do
+        sign_in user
+        get :create, params: { body: 'test' }
+      end
+
+      # pending 'Not update test yet.'
+      its(:response) {is_expected.to have_http_status(302)}
+      its(:response) {is_expected.to redirect_to(:root)}
+      it { should set_flash[:notice].to('Post was successfully created.')}
     end
   end
 
