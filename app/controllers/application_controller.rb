d42f3bbc505d69f6671b516a8562578b2608c5eb
sonatine726 <sonatine726@gmail.com>
2018-06-01 19:21:47 +0900

    Separate error handling from ApplicationController to ErrorHandlers module.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  rescue_from SecurityError do |exception|
-    redirect_to root_url, notice: 'Admin画面へのアクセス権限がありません'
-  end
-
-  rescue_from ForbiddenError, with: :rescue403
-  rescue_from IpAddressRejectedError, with: :rescue403
-  rescue_from ActionController::RoutingError, with: :rescue404
-  rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  include ErrorHandlers if Rails.env.production?
 
   protected
-
   def configure_permitted_parameters
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me,:avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
@@ -22,17 +14,5 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin_user!
     raise SecurityError unless current_user.try(:admin?)
-  end
-
-  private
-
-  def rescue403(e)
-    @exception = e
-    render 'errors/forbidden_error', status: 403
-  end
-
-  def rescue404(e)
-    @exception = e
-    render 'errors/not_found_error', status: 404
   end
 end
