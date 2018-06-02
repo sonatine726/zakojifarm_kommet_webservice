5ba7fa45201716e14fab14429e6e395350749c47
sonatine726 <sonatine726@gmail.com>
2018-06-02 16:38:40 +0900

    Migrate a table named staff_members.

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -1,3 +1,22 @@
+# == Schema Information
+#
+# Table name: staff_members
+#
+#  id               :bigint(8)        not null, primary key
+#  email            :string           not null
+#  email_for_index  :string           not null
+#  family_name      :string           not null
+#  given_name       :string           not null
+#  family_name_kana :string           not null
+#  given_name_kana  :string           not null
+#  hashed_password  :string
+#  start_date       :date             not null
+#  end_date         :date
+#  suspended        :boolean          default(FALSE), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe StaffMember, type: :model do
