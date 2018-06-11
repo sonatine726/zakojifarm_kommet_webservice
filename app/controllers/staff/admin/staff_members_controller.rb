bb2be4d0c7ba0fdbba440f7edbfaf3664d90e099
sonatine726 <sonatine726@gmail.com>
2018-06-11 18:51:29 +0900

    Implement functions "create", "update", and "destroy" in class 'Staff::Admin::StaffMembersController'.

--- app/controllers/staff/admin/staff_members_controller.rb
+++ app/controllers/staff/admin/staff_members_controller.rb
@@ -18,4 +18,56 @@ class Staff::Admin::StaffMembersController < Staff::Admin::Base
     @title = '職員アカウントの編集'
     @staff_member = StaffMember.find(params[:id])
   end
+
+  def create
+    @staff_member = StaffMember.new(create_params)
+    if @staff_member.save
+      flash.notice = '職員アカウントを新規登録しました。'
+      redirect_to :admin_staff_members
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    @staff_member = StaffMember.find(params[:id])
+    @staff_member.assign_attributes(update_params)
+    if @staff_member.save
+      flash.notice = '職員アカウントを更新しました。'
+      redirect_to :admin_staff_members
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    staff_member = StaffMember.find(params[:id])
+    staff_member.destroy!
+    flash.notice = '職員アカウントを削除しました。'
+    redirect_to :admin_staff_members
+  end
+
+  protected
+  def create_params
+    params.require(:staff_member).permit(:email, 
+                                         :password, 
+                                         :family_name,
+                                         :given_name, 
+                                         :family_name_kana, 
+                                         :given_name_kana, 
+                                         :start_date, 
+                                         :end_date, 
+                                         :suspended)
+  end
+
+  def update_params
+    params.require(:staff_member).permit(:email, 
+                                         :family_name,
+                                         :given_name, 
+                                         :family_name_kana, 
+                                         :given_name_kana, 
+                                         :start_date, 
+                                         :end_date, 
+                                         :suspended)
+  end
 end
