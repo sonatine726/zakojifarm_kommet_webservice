fa6f849854d1d3f9c4cc329f1495271222061be7
sonatine726 <sonatine726@gmail.com>
2018-06-11 09:46:58 +0900

    Add routes for admin/staff_members.

--- app/controllers/staff/admin/top_controller.rb
+++ app/controllers/staff/admin/top_controller.rb
@@ -1,6 +1,11 @@
 class Staff::Admin::TopController < Staff::Admin::Base
   def index
-    @title = '管理者トップページ'
-    render action: 'index'
+    if current_admin_member
+      @title = '管理者ダッシュボード'
+      render action: 'dashboard'
+    else
+      @title = '管理者トップページ'
+      render action: 'index'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
+    resources :staff_members
   end
 
   get '*anything' => 'errors#routing_error'
