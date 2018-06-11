f0161cc5dc17bd0526668696f4c1b1c7ce840955
sonatine726 <sonatine726@gmail.com>
2018-06-11 18:24:31 +0900

    Add a form for new staff member to admin.

--- app/controllers/staff/admin/staff_members_controller.rb
+++ app/controllers/staff/admin/staff_members_controller.rb
@@ -1,5 +1,16 @@
 class Staff::Admin::StaffMembersController < Staff::Admin::Base
   def index
+    @title = '職員管理'
     @staff_members = StaffMember.order(:family_name_kana, :given_name_kana)
+  end
+
+  def show
+    staff_member = StaffMember.find(param[:id])
+    redirect_to [:edit, :admin, staff_member]
+  end
+
+  def new
+    @title = '職員の新規登録'
+    @staff_member = StaffMember.new
   end
 end
