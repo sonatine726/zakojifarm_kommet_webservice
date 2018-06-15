a9c71b7c77916e01633d10b15c40e370b01ced22
sonatine726 <sonatine726@gmail.com>
2018-06-15 12:26:43 +0900

    Add tests for admin/sessions_controller.

--- spec/factories/admin_members.rb
+++ spec/factories/admin_members.rb
@@ -19,6 +19,14 @@
 
 FactoryBot.define do
   factory :admin_member do
-    
+    email 'admin@example.com'
+    family_name '山田'
+    given_name '太郎'
+    family_name_kana 'ヤマダ'
+    given_name_kana 'タロウ'
+    password 'admin'
+    start_date Date.today
+    end_date nil
+    suspended false
   end
 end
