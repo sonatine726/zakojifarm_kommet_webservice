0db1e2546c30b1ed5434ee137c879c806f3ff1f0
sonatine726 <sonatine726@gmail.com>
2018-06-15 12:34:34 +0900

    Add tests for admin/staff_members_controller.

--- spec/controllers/staff/admin/staff_members_controller_spec.rb
+++ spec/controllers/staff/admin/staff_members_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Staff::Admin::StaffMembersController, type: :controller do
-  pending
+  let(:params_hash){attributes_for(:staff_member)}
+  before do
+    admin_for_test = create(:admin_member)
+    session[:admin_member_id] = admin_for_test.id
+  end
+
+  describe "#create" do
+    it '職員一覧ページにリダイレクト' do
+      post :create, params: { staff_member: params_hash }
+      expect(response).to redirect_to(admin_staff_members_url)
+    end
+
+    it '例外ActionController::ParameterMissingが発生' do
+      bypass_rescue
+      expect{post :create}.to raise_error(ActionController::ParameterMissing)
+    end
+  end
 end
