ddabb800da49d436ed74f6c04020f6201afe7289
sonatine726 <sonatine726@gmail.com>
2018-06-15 10:25:19 +0900

    Commit intermediate.(#165 should be implemented for passing this test. So pending this branch until #165 finished.)

--- spec/controllers/staff/admin/staff_members_controller_spec.rb
+++ spec/controllers/staff/admin/staff_members_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Staff::Admin::StaffMembersController, type: :controller do
+  let(:params_hash){attributes_for(:staff_member)}
 
+  describe "#create" do
+    it '職員一覧ページにリダイレクト' do
+      post :create, staff_member: params_hash
+      expect(response).to redirect_to(admin_staff_members_url)
+    end
+
+    it '例外ActionController::ParameterMissingが発生' do
+      bypass_rescue
+      expect{post :create}.to raise_error(ActionController::ParameterMissing)
+    end
+  end
 end
