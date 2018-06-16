57d376857e7de072914004a4d803dd2f463fda9a
sonatine726 <sonatine726@gmail.com>
2018-06-16 11:15:41 +0900

    Add a function to edit staff account by own self.

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -1,5 +1,11 @@
 class Staff::AccountsController < Staff::Base
   def show
+    @title = 'アカウント情報'
+    @staff_member = current_staff_member
+  end
+
+  def edit
+    @title = 'アカウント情報編集'
     @staff_member = current_staff_member
   end
 end
