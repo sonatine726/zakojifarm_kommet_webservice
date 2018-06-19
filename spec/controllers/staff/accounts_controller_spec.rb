33e1d05e4208e6e209ff4dbf9cc8955ec9dd5b4a
sonatine726 <sonatine726@gmail.com>
2018-06-19 09:57:07 +0900

    Add tests for Staff::AccountsController#update.

--- spec/controllers/staff/accounts_controller_spec.rb
+++ spec/controllers/staff/accounts_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Staff::AccountsController, type: :controller do
+  describe '#update' do
+    let(:params_hash){attributes_for(:staff_member)}
+    let(:staff_member){create(:staff_member)}
+    before do
+      session[:staff_member_id] = staff_member.id
+    end
 
+    it 'email属性を変更' do
+      params_hash.merge!(email: 'test@example.com')
+      patch :update, params: {id: staff_member.id, staff_member:params_hash}
+      staff_member.reload
+      expect(staff_member.email).to eq('test@example.com')
+    end
+
+    it '例外ActionController::ParameterMissingが発生' do
+      bypass_rescue
+      expect{ patch :update, params: {id: staff_member.id} }.
+      to raise_error(ActionController::ParameterMissing)
+    end
+
+    it 'end_dateの値は書き換え不可能' do
+      params_hash.merge!(end_date: Date.tomorrow)
+      expect{
+        patch :update, params: {id: staff_member.id, staff_member: params_hash}
+      }.not_to change{ staff_member.end_date }
+    end
+  end
 end
