0423981062718eaf8c071b0dd064488b1475d606
sonatine726 <sonatine726@gmail.com>
2018-06-19 11:45:57 +0900

    Add tests for force logout to suspended abount Staff.

---
+++ spec/controllers/staff/top_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Staff::TopController, type: :controller do
+  context 'ログイン後' do
+    let(:staff_member){create(:staff_member)}
+    before do
+      session[:staff_member_id] = staff_member.id
+      session[:last_access_time] = 1.second.ago
+    end
+
+    describe '#index' do
+      it '通常はstaff/top/indexを表示' do
+        get :index
+        expect(response).to render_template('staff/top/index')
+      end
+
+      it '停止フラグがセットされたら強制ログアウト' do
+        staff_member.update_column(:suspended, true)
+        get :index
+        expect(session[:staff_member_id]).to be_nil
+        expect(response).to redirect_to(staff_login_url)
+      end
+    end
+  end
+end
