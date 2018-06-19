89bb11cb59631db12ada930b263d93f756f56950
sonatine726 <sonatine726@gmail.com>
2018-06-19 11:03:04 +0900

    Add tests for before login to Staff::Admin::StaffMembersController.

---
+++ spec/support/shared_examples_for_admin_controllers.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+RSpec.shared_examples 'a protected admin controller' do
+  describe '#index' do
+    it 'ログインフォームにリダイレクト' do
+      get :index
+      expect(response).to redirect_to(admin_login_url)
+    end
+  end
+
+  describe '#show' do
+    it 'ログインフォームにリダイレクト' do
+      get :show, params: {id:1}
+      expect(response).to redirect_to(admin_login_url)
+    end
+  end
+end
+
+RSpec.shared_examples 'a protected singular admin controller' do
+  describe '#show' do
+    it 'ログインフォームにリダイレクト' do
+      get :show
+      expect(response).to redirect_to(admin_login_url)
+    end
+  end
+end
