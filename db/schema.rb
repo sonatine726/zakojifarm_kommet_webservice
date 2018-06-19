1330c4132f26857cb39c7e9509fc4be6634851c6
sonatine726 <sonatine726@gmail.com>
2018-06-19 21:03:01 +0900

    Migrate db table staff_events.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606020604) do
+ActiveRecord::Schema.define(version: 20180619095711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20180606020604) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "staff_events", force: :cascade do |t|
+    t.bigint "staff_member_id", null: false
+    t.string "type", null: false
+    t.datetime "created_at", null: false
+    t.index ["created_at"], name: "index_staff_events_on_created_at"
+    t.index ["staff_member_id", "created_at"], name: "index_staff_events_on_staff_member_id_and_created_at"
+    t.index ["staff_member_id"], name: "index_staff_events_on_staff_member_id"
   end
 
   create_table "staff_members", force: :cascade do |t|
@@ -90,4 +99,5 @@ ActiveRecord::Schema.define(version: 20180606020604) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "staff_events", "staff_members"
 end
