9e1b137a12b057879c76fc70b602952f2101f8df
sonatine726 <sonatine726@gmail.com>
2018-06-20 15:24:22 +0900

    - Fix N+1 problem in staff_events_controller
    - Refactor staff_events_controller

--- app/controllers/staff/admin/staff_events_controller.rb
+++ app/controllers/staff/admin/staff_events_controller.rb
@@ -2,11 +2,11 @@ class Staff::Admin::StaffEventsController < Staff::Admin::Base
   def index
     if params[:staff_member_id]
       @staff_member = StaffMember.find(params[:staff_member_id])
-      @events = @staff_member.events.order(occurred_at: :desc)
+      @events = @staff_member.events
     else
-      @events = StaffEvent.order(occurred_at: :desc)
+      @events = StaffEvent
     end
-    @events = @events.page(params[:page])
+    @events = @events.order(occurred_at: :desc).includes(:member).page(params[:page])
 
     if @staff_member
       full_name = @staff_member.family_name + @staff_member.given_name
