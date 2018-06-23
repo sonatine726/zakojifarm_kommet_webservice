716c5d77a5b65a52a9162cee71d4f2d9e922afce
sonatine726 <sonatine726@gmail.com>
2018-06-23 11:12:08 +0900

    Additionaly refactor staff_member_form by using staff_member_form_presenter

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -9,4 +9,38 @@ class FormPresenter
     @form_builder = form_builder
     @view_context = view_context
   end
+
+  def notes
+    markup(:small, class: 'notes') do |m|
+      m.span '*', class: 'mark'
+      m.text '印の付いた項目は入力必須です。'
+    end
+  end
+
+  def text_field_block(name, label_text, **options)
+    markup(:div, class: options[:class_top_div]) do |m|
+      m << label(name, label_text, class: "#{options[:required] ? 'required' : nil}, #{options[:class_label]}")
+      m.div(class: options[:class_between_label_and_text]) do |m|
+        m << text_field(name, class: options[:class_textf], size: options[:textf_size])
+      end
+    end
+  end
+
+  def password_field_block(name, label_text, **options)
+    markup(:div, class: options[:class_top_div]) do |m|
+      m << label(name, label_text, class: "#{options[:required] ? 'required' : nil}, #{options[:class_label]}")
+      m.div(class: options[:class_between_label_and_password]) do |m|
+        m << password_field(name, class: options[:class_passwordf], size: options[:passwordf_size])
+      end
+    end
+  end
+
+  def date_field_block(name, label_text, **options)
+    markup(:div, class: options[:class_top_div]) do |m|
+      m << label(name, label_text, class: "#{options[:required] ? 'required' : nil}, #{options[:class_label]}")
+      m.div(class: options[:class_between_label_and_text]) do |m|
+        m << text_field(name, class: options[:class_textf], size: options[:textf_size])
+      end
+    end
+  end
 end
--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -1,12 +1,25 @@
 class StaffMemberFormPresenter < FormPresenter
+  def password_field_block(name, label_text, **options)
+    if object.new_record?
+      super(name, label_text, options)
+    end
+  end
+
   def full_name_block(name1, name2, label_text, **options)
-    markup do |m|
-      m.div(class: options[:class_top_div]) do |m|
-        m << label(name1, label_text, class: "#{options[:required] ? 'required' : nil}, #{options[:class_label]}")
-        m.div(class: options[:class_between_label_and_text]) do |m|
-          m << text_field(name1, class: options[:class_textf_1], size: options[:textf_size])
-          m << text_field(name2, class: options[:class_textf_2], size: options[:textf_size])
-        end
+    markup(:div, class: options[:class_top_div]) do |m|
+      m << label(name1, label_text, class: "#{options[:required] ? 'required' : nil}, #{options[:class_label]}")
+      m.div(class: options[:class_between_label_and_text]) do |m|
+        m << text_field(name1, class: options[:class_textf_1], size: options[:textf_size])
+        m << text_field(name2, class: options[:class_textf_2], size: options[:textf_size])
+      end
+    end
+  end
+
+  def check_boxes(name, label_text, **options)
+    markup(:div, class: options[:class_top_div]) do |m|
+      m.div(class: options[:class_before_checkbox]) do |m|
+        m << check_box(name, class: options[:class_checkbox])
+        m << label(name, label_text)
       end
     end
   end
