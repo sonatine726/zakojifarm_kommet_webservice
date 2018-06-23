c36d95e72c1306b433b5e7bfe214d391d7be83fd
sonatine726 <sonatine726@gmail.com>
2018-06-23 12:33:53 +0900

    Display error messages on new_admin_staff_member view

--- config/application.rb
+++ config/application.rb
@@ -41,5 +41,6 @@ module App
     # i18n
     config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
