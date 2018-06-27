3c23c95a692d77a705b12e04528c08edcf5cb3c8
sonatine726 <sonatine726@gmail.com>
2018-06-27 12:20:01 +0900

    Cherry pic 7fe1d299d0310b874eaecfc797ddb87b8c343911 [7fe1d29]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -21,8 +21,8 @@ class Customer < ApplicationRecord
   include PersonalNameHolder
   include PasswordHolder
 
-  has_one :home_address, dependent: :destroy
-  has_one :work_address, dependent: :destroy
+  has_one :home_address, dependent: :destroy, autosave: true
+  has_one :work_address, dependent: :destroy, autosave: true
 
   validates :gender,inclusion: { in: %w(male female), allow_blank: true }
   validates :birthday, date: {
