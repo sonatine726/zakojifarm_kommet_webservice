de07cd1700e9e773da56060d1cea1ecc41cb9d0c
sonatine726 <sonatine726@gmail.com>
2018-07-02 19:19:58 +0900

    Add a function to view staff_admin_allowed_sources_index

---
+++ app/controllers/staff/admin/allowed_sources_controller.rb
@@ -0,0 +1,6 @@
+class Staff::Admin::AllowedSourcesController < Staff::Admin::Base
+  def index
+    @title = "許可IPアドレス一覧"
+    @allowed_sources = AllowedSource.order(:octet1, :octet2, :octet3, :octet4)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,9 @@ Rails.application.routes.draw do
         resources :staff_events, only: [ :index ]
       end
       resources :staff_events, only: [ :index ]
+      resources :allowed_sources, only: [ :index, :create ] do
+        delete :delele, on: :collection
+      end
     end
   end
 
