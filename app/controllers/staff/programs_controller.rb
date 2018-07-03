167ad8986768481f5adfbd444f586dcaa9fbd83f
sonatine726 <sonatine726@gmail.com>
2018-07-03 16:38:05 +0900

    Commit intermediate ( Add performance tests for view staff_programs_index)

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -3,4 +3,9 @@ class Staff::ProgramsController < Staff::Base
     @title = 'プログラム管理'
     @programs = Program.order(application_start_time: :desc).page(params[:page])
   end
+
+  def show
+    @title = 'プログラム詳細情報'
+    @program = Program.find(params[:id])
+  end
 end
