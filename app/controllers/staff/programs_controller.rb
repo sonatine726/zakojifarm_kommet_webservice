8be24d98c029bb0bf1861b041d6afe86ad1b222f
sonatine726 <sonatine726@gmail.com>
2018-07-04 17:25:45 +0900

    Add functions create and update to Program

--- app/controllers/staff/programs_controller.rb
+++ app/controllers/staff/programs_controller.rb
@@ -18,4 +18,30 @@ class Staff::ProgramsController < Staff::Base
     @title = 'プログラムの情報更新'
     @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
   end
+
+  def create
+    @program_form = Staff::ProgramForm.new
+    @program_form.assign_attributes(params[:form])
+    @program_form.program.registrant = current_staff_member
+    if @program_form.save
+      flash.notice = 'プログラムを登録しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  def update
+    @program_form = Staff::ProgramForm.new(Program.find(params[:id]))
+    @program_form.assign_attributes(params[:form])
+    @program_form.program.registrant = current_staff_member
+    if @program_form.save
+      flash.notice = 'プログラムを更新しました。'
+      redirect_to action: 'index'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'edit'
+    end
+  end
 end
