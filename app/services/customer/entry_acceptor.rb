69e6a3f1238df8cdad2e8dca165121a92c845708
sonatine726 <sonatine726@gmail.com>
2018-07-06 10:25:10 +0900

    Add a function to cancel entry to program

--- app/services/customer/entry_acceptor.rb
+++ app/services/customer/entry_acceptor.rb
@@ -4,7 +4,12 @@ class Customer::EntryAcceptor
   end
 
   def accept(program)
-    if max = program.max_number_of_participants
+    raise if Time.current < program.application_start_time
+
+    return :closed if Time.current >= program.application_end_time
+
+    if program.entries.where(customer_id: @customer.id).exists? == false &&
+      max = program.max_number_of_participants
       if program.entries.where(canceled: false).count >= max
         return :full
       end
